49f42f2b6a2257e6900bf5476e278c4d6823b153
Malik Samansya <maliksamansya206@gmail.com>
2023-08-31 15:16:13 +0700

    refactor: create Landlord table

---
+++ client/client-admin/src/components/LandlordsTable.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+const LandlordsTable = () => {
+  const [users, setUsers] = useState([
+    { id: 1, email: 'example1@example.com', phoneNumber: '123-456-7890', address: '123 Example Street' },
+    { id: 2, email: 'example2@example.com', phoneNumber: '987-654-3210', address: '456 Example Avenue' },
+    // add more users as needed
+  ]);
+
+  return (
+    <table className="table table-striped">
+      <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Email</th>
+        <th scope="col">Phone Number</th>
+        <th scope="col">Address</th>
+        <th scope="col">tabelnya Landlords</th>
+      </tr>
+      </thead>
+      <tbody>
+      {users.map((user) => (
+        <tr key={user.id}>
+          <th scope="row">{user.id}</th>
+          <td>{user.email}</td>
+          <td>{user.phoneNumber}</td>
+          <td>{user.address}</td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default LandlordsTable
